57d39051ffb157bd49df3059ac983e71f9b71bdf
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2024-11-17 00:06:56 +0700

    refactor: make phone number user is option data

--- database/migrations/1730125757988-CreateUserTable.ts
+++ database/migrations/1730125757988-CreateUserTable.ts
@@ -14,7 +14,6 @@ export class CreateUserTable1730125757988 implements MigrationInterface {
                 "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
                 "deleted_at" TIMESTAMP,
                 CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"),
-                CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("phone_number"),
                 CONSTRAINT "PK_cace4a159ff9f2512dd423907f2" PRIMARY KEY ("id")
             )
         `)
